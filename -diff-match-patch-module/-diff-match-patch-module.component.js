7f3d032ad2d49f7f0d28b11b91274432f47e54ab
aclark23 <alex.clark@veriday.com>
2017-10-18 11:13:39 -0400

    fixing some things.

---
+++ -diff-match-patch-module/-diff-match-patch-module.component.js
@@ -0,0 +1,12 @@
+class DiffMatchPatchModuleController {
+    constructor() {
+    }
+}
+
+export default {
+    controller: DiffMatchPatchModuleController,
+    template: require('./-diff-match-patch-module.html'),
+    bindings: {
+        
+    }
+};
